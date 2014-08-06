14169aeef673301c41856f4aac00506e2c3db5a9
Audrius Siliunas <audrius.siliunas@tietocamp.eu>
2014-08-06 14:27:08 +0300

    user interface workout packaged created

---
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/workout/WorkoutTopListPanel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tieto.it2014.ui.workout;
+
+/**
+ *
+ * @author pc4
+ */
+public class WorkoutTopListPanel {
+    
+}

aa39eb18c90e5c50ece73cd8b0c7ca99979e9637
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-07 19:45:09 +0300

    Dummy list

--- it2014-dao/src/main/java/com/tieto/it2014/dao/workout/query/AllWorkoutsQueryDaoMem.java
+++ it2014-dao/src/main/java/com/tieto/it2014/dao/workout/query/AllWorkoutsQueryDaoMem.java
@@ -4,6 +4,7 @@ import com.tieto.it2014.dao.workout.WorkoutEntity;
 import com.tieto.it2014.dao.workout.WorkoutEntityRepository;
 import com.tieto.it2014.domain.Util.Util;
 import com.tieto.it2014.domain.user.entity.UserLoc;
+import com.tieto.it2014.domain.user.entity.Workout;
 import com.tieto.it2014.domain.workout.query.WorkoutsQuery;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,30 +20,40 @@ public class AllWorkoutsQueryDaoMem implements WorkoutsQuery.Dao {
     int sec = 0;
     int totalSec = 0;
     int k = 0;
+    Long start;
+    Long end;
+    Boolean st = true;
 
     @Autowired
     private WorkoutEntityRepository repository;
 
     @Override
     @Transactional(readOnly = true)
-    public List<UserLoc> result() {
-        List<UserLoc> users = new ArrayList<>();
+    public List<Workout> result() {
+        List<Workout> users = new ArrayList<>();
         List<WorkoutEntity> we = repository.all();
         for (int i = 0; i < we.size()-2 ; i++) {
             WorkoutEntity lc1 = we.get(i);
             WorkoutEntity lc2 = we.get(i+1);
+            if (st) {
+                end = lc1.getTimeStamp();
+                st = false;
+            }
             sec = (int)Util.calculateDuration(lc2.getTimeStamp(), lc1.getTimeStamp());
             if (sec < 300 && lc2.getPhoneNumber().equals(lc1.getPhoneNumber()) && !lc2.equals(we.get(we.size()-1))) {
                 totalSec += sec;
                 k++;
             } else {
-                System.out.println("Total time: " + totalSec + ", WorkoutID: " + ++workoutId + " " + lc1.getPhoneNumber() + ", TS: " + k);
+                start = lc1.getTimeStamp();
+                st = true;
+                System.out.println("Total time: " + totalSec + ", WorkoutID: " + ++workoutId + " " + start + " " + end);
                 totalSec = 0;
                 k = 0;
             }
-            users.add(lc1.toWorkout());
         }
-        users.add(we.get(we.size() - 1).toWorkout());
+        users.add(new Workout(1L, "123456789", 10L, 20L, 30L, 40L, "User"));
+        users.add(new Workout(2L, "123456789", 10L, 20L, 30L, 40L, "User2"));
+        users.add(new Workout(3L, "123456789", 10L, 20L, 30L, 40L, "User3"));
         return users;
     }
 
--- it2014-domain/src/main/java/com/tieto/it2014/domain/workout/query/WorkoutsQuery.java
+++ it2014-domain/src/main/java/com/tieto/it2014/domain/workout/query/WorkoutsQuery.java
@@ -3,6 +3,8 @@ package com.tieto.it2014.domain.workout.query;
 import com.tieto.it2014.domain.user.entity.UserLoc;
 import java.io.Serializable;
 import java.util.List;
+
+import com.tieto.it2014.domain.user.entity.Workout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,10 @@ public class WorkoutsQuery implements Serializable {
     private Dao dao;
 
     public interface Dao extends Serializable {
-        List<UserLoc> result();
+        List<Workout> result();
     }
 
-    public List<UserLoc> result() {
+    public List<Workout> result() {
         return dao.result();
     }
 

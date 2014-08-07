4be787ee25e94de216d4a52f0b93240e269241c6
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-07 19:12:28 +0300

    Basic algorithm implementation

--- it2014-dao/src/main/java/com/tieto/it2014/dao/workout/query/AllWorkoutsQueryDaoMem.java
+++ it2014-dao/src/main/java/com/tieto/it2014/dao/workout/query/AllWorkoutsQueryDaoMem.java
@@ -2,6 +2,7 @@ package com.tieto.it2014.dao.workout.query;
 
 import com.tieto.it2014.dao.workout.WorkoutEntity;
 import com.tieto.it2014.dao.workout.WorkoutEntityRepository;
+import com.tieto.it2014.domain.Util.Util;
 import com.tieto.it2014.domain.user.entity.UserLoc;
 import com.tieto.it2014.domain.workout.query.WorkoutsQuery;
 import java.util.ArrayList;
@@ -14,6 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class AllWorkoutsQueryDaoMem implements WorkoutsQuery.Dao {
 
     private static final long serialVersionUID = 1L;
+    int workoutId = 0;
+    int sec = 0;
+    int totalSec = 0;
+    int k = 0;
 
     @Autowired
     private WorkoutEntityRepository repository;
@@ -22,9 +27,22 @@ public class AllWorkoutsQueryDaoMem implements WorkoutsQuery.Dao {
     @Transactional(readOnly = true)
     public List<UserLoc> result() {
         List<UserLoc> users = new ArrayList<>();
-        for (WorkoutEntity entity : repository.all()) {
-            users.add(entity.toWorkout());
+        List<WorkoutEntity> we = repository.all();
+        for (int i = 0; i < we.size()-2 ; i++) {
+            WorkoutEntity lc1 = we.get(i);
+            WorkoutEntity lc2 = we.get(i+1);
+            sec = (int)Util.calculateDuration(lc2.getTimeStamp(), lc1.getTimeStamp());
+            if (sec < 300 && lc2.getPhoneNumber().equals(lc1.getPhoneNumber()) && !lc2.equals(we.get(we.size()-1))) {
+                totalSec += sec;
+                k++;
+            } else {
+                System.out.println("Total time: " + totalSec + ", WorkoutID: " + ++workoutId + " " + lc1.getPhoneNumber() + ", TS: " + k);
+                totalSec = 0;
+                k = 0;
+            }
+            users.add(lc1.toWorkout());
         }
+        users.add(we.get(we.size() - 1).toWorkout());
         return users;
     }
 

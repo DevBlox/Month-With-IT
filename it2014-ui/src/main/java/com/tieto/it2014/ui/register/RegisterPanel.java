f24a17b0ac94e34d941726d1c52b5158793754cc
Lukas Praninskas <lukas.praninskas@tietocamp.eu>
2014-08-08 12:32:36 +0300

    Working build

--- it2014-ui/src/main/java/com/tieto/it2014/ui/register/RegisterPanel.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/register/RegisterPanel.java
@@ -3,7 +3,10 @@ package com.tieto.it2014.ui.register;
 import com.tieto.it2014.domain.user.command.CreateUserCommand;
 import com.tieto.it2014.domain.user.command.SaveUserCommand;
 import com.tieto.it2014.domain.user.entity.User;
+import com.tieto.it2014.domain.user.query.AllUsersQuery;
 import com.tieto.it2014.ui.HomePage;
+import java.util.List;
+import java.util.Objects;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
@@ -21,29 +24,31 @@ public class RegisterPanel extends Panel {
 
     private static final long serialVersionUID = 1L;
 
-    
-    
     public RegisterPanel(String wicketid) {
         super(wicketid);
     }
-    
+
     private User user;
+    private Form form;
 
     @SpringBean
     private CreateUserCommand createUser;
-    
+
     @SpringBean
     private SaveUserCommand saveUser;
 
+    @SpringBean
+    private AllUsersQuery allUsersQuery;
+
     @Override
     protected void onInitialize() {
-        
+
         super.onInitialize();
         user = createUser.getUser();
-        Form form = new Form("registerForm");
+        form = new Form("registerForm");
         PasswordTextField password = new PasswordTextField("inputPassword", new PropertyModel(user, "password"));
         PasswordTextField repeatPassword = new PasswordTextField("repeatPassword", new PropertyModel(user, "password"));
-        
+
         form.add(new FeedbackPanel("registerFeedback"));
         form.add(new TextField("inputUserName", new PropertyModel(user, "username"))
                 .setRequired(true)
@@ -62,22 +67,34 @@ public class RegisterPanel extends Panel {
                 .add(EmailAddressValidator.getInstance())
         );
         form.add(new TextField("imei", new PropertyModel(user, "imei"))
-            .setRequired(true));
+                .setRequired(true));
         form.add(initRegisterButton("registerButton"));
         form.add(new EqualPasswordInputValidator(password, repeatPassword));
-        //form.add(new ExistingUserValidator());
         add(form);
     }
-    
+
     private Component initRegisterButton(String wicketId) {
         return new Button(wicketId) {
             private static final long serialVersionUID = 1L;
 
             @Override
             public void onSubmit() {
-                saveUser.execute(user);
-                setResponsePage(HomePage.class);
+                actionRegisterUser();
             }
+
         };
+    }
+
+    private void actionRegisterUser() {
+        List<User> userList = allUsersQuery.result();
+        for (User usr : userList) {
+            if (Objects.equals(usr.imei, user.imei)) {
+                form.error("This imei is already in use!");
+            }
+        }
+        if (!form.hasErrorMessage()) {
+            saveUser.execute(user);
+            setResponsePage(HomePage.class);
+        }
     }
 }

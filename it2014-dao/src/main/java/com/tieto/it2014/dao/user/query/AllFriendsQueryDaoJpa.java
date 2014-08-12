33091fd9a53f3001f617763122d94e5a6e49cc0e
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-12 13:01:09 +0300

    changelog change

--- it2014-dao/src/main/java/com/tieto/it2014/dao/user/query/AllFriendsQueryDaoJpa.java
+++ it2014-dao/src/main/java/com/tieto/it2014/dao/user/query/AllFriendsQueryDaoJpa.java
@@ -1,5 +1,6 @@
 package com.tieto.it2014.dao.user.query;
 
+import com.tieto.it2014.dao.user.UserJpa;
 import com.tieto.it2014.domain.user.entity.User;
 import com.tieto.it2014.domain.user.query.AllFriendsQuery;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Component
@@ -20,6 +22,9 @@ public class AllFriendsQueryDaoJpa implements AllFriendsQuery.Dao {
     @Override
     @Transactional(readOnly = true)
     public List<User> result() {
+        String hql = "SELECT f from FriendJpa f INNER JOIN UserJpa u WHERE f.friendId = u.imei ORDER BY u.username";
+        Query query = em.createQuery(hql, UserJpa.class);
+        List results = query.getResultList();
         return null;
     }
 }
---
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/user/UserFriendsPage.java
@@ -0,0 +1,48 @@
+package com.tieto.it2014.ui.user;
+
+import com.tieto.it2014.domain.user.entity.User;
+import com.tieto.it2014.domain.user.query.AllFriendsQuery;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.List;
+
+public class UserFriendsPage extends WebPage {
+    private static final long serialVersionUID = 1L;
+    public static final String USER_ID = "userId";
+    private final String userId;
+
+    @SpringBean
+    private AllFriendsQuery allFriendsQuery;
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(new Label("name", userId));
+        IModel<List<User>> userFriendsModel = initUserFriendsListModel();
+        RepeatingView view = new RepeatingView("item");
+        for (Object wk : userFriendsModel.getObject()) {
+            System.out.println(wk);
+        }
+    }
+
+    public UserFriendsPage(PageParameters params) {
+        userId = params.get(USER_ID).toString();
+    }
+
+    private IModel<List<User>> initUserFriendsListModel() {
+        return new LoadableDetachableModel<List<User>>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected List<User> load() {
+                return allFriendsQuery.result();
+            }
+        };
+    }
+}

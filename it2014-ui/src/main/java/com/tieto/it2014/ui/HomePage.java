87a66ed5326b51fb5743cb545b8db60873db1cbc
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-13 16:07:04 +0300

    My workouts

--- it2014-ui/src/main/java/com/tieto/it2014/ui/HomePage.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/HomePage.java
@@ -3,8 +3,10 @@ package com.tieto.it2014.ui;
 import com.tieto.it2014.ui.friend.FriendPanel;
 import com.tieto.it2014.ui.header.HeaderPanel;
 import com.tieto.it2014.ui.session.UserSession;
+import com.tieto.it2014.ui.workout.UserWorkoutPanel;
 import com.tieto.it2014.ui.workout.WorkoutTopListPanel;
 import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
@@ -12,25 +14,21 @@ public class HomePage extends BasePage {
 
     private static final long serialVersionUID = 1L;
 
-    private FriendPanel friendPanel;
-    private WorkoutTopListPanel workoutTopListPanel;
-    private Label label;
-
-    @Override
-    protected void onInitialize() {
-        super.onInitialize();
-        initContent(CONTENT_ID);
-//        friendPanel = new FriendPanel("friendPanel");
-//        workoutTopListPanel = ;
-//        label = new Label("Heading", "Recent workouts");
-//        add(friendPanel);
-//        add(label);
-//        add(workoutTopListPanel);
-    }
+//    @Override
+//    protected void onInitialize() {
+//        super.onInitialize();
+////        friendPanel = new FriendPanel("friendPanel");
+////        workoutTopListPanel = ;
+////        label = new Label("Heading", "Recent workouts");
+////        add(friendPanel);
+////        add(label);
+////        add(workoutTopListPanel);
+//    }
 
     @Override
     protected void onConfigure() {
         super.onConfigure();
+        initContent(CONTENT_ID);
 //        friendPanel.setVisible(UserSession.get().hasUser());
 //        workoutTopListPanel.setVisible(!UserSession.get().hasUser());
 //        label.setVisible(!UserSession.get().hasUser());
@@ -38,6 +36,6 @@ public class HomePage extends BasePage {
 
     @Override
     protected Component initContent(String wicketId) {
-        return new WorkoutTopListPanel(wicketId);
+        return UserSession.get().getUser() == null ? new WorkoutTopListPanel(wicketId) : new UserWorkoutPanel(wicketId, UserSession.get().getUser().imei);
     }
 }

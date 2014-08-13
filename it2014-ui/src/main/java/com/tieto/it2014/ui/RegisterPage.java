fe9fd0a956c8d2e49cee8b64b214ad6a3ea0c1fe
Lukas Praninskas <lukas.praninskas@tietocamp.eu>
2014-08-13 17:39:00 +0300

    Merge

--- it2014-ui/src/main/java/com/tieto/it2014/ui/RegisterPage.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/RegisterPage.java
@@ -7,20 +7,25 @@ package com.tieto.it2014.ui;
 
 import com.tieto.it2014.ui.header.HeaderPanel;
 import com.tieto.it2014.ui.register.RegisterPanel;
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
  *
  * @author pc3
  */
-public class RegisterPage extends WebPage {
+public class RegisterPage extends BasePage {
 
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        add(new RegisterPanel("register"));
-        add(new HeaderPanel("header"));
+        initContent(CONTENT_ID);
+    }
+
+    @Override
+    protected Component initContent(String wicketId) {
+        return new RegisterPanel(wicketId);
     }
 }

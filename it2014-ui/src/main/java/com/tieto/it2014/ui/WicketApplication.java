973f9eaa4e8b7bd3253131f3fd23726ab3e5f820
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-14 04:07:24 +0300

    404 page added.

--- it2014-ui/src/main/java/com/tieto/it2014/ui/WicketApplication.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/WicketApplication.java
@@ -1,5 +1,6 @@
 package com.tieto.it2014.ui;
 
+import com.tieto.it2014.ui.error.ErrorPage404;
 import com.tieto.it2014.ui.session.UserSession;
 import com.tieto.it2014.ui.user.UserFriendsPage;
 import com.tieto.it2014.ui.user.UserWorkoutsPage;
@@ -30,6 +31,7 @@ public class WicketApplication extends WebApplication implements ApplicationCont
         getComponentInstantiationListeners().add(new SpringComponentInjector(this, this.context, true));
         getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
         getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
+        mountPage("error404",ErrorPage404.class);
         // http://apache-wicket.1842946.n4.nabble.com/How-Runtime-Exception-Handling-td1888907.html
         //getApplicationSettings().setPageExpiredErrorPage(PageExpiredErrorPage.class);
         //getApplicationSettings().setAccessDeniedPage(AccessDeniedPage.class);
---
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/error/ErrorPage404.java
@@ -0,0 +1,14 @@
+package com.tieto.it2014.ui.error;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ErrorPage404 extends WebPage {
+
+    public ErrorPage404(final PageParameters parameters) {
+
+        add(new Label("404", "Page Not Found!"));
+
+    }
+}

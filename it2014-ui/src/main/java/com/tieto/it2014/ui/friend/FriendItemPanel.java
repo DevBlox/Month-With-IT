4cf8c05a6eb313e0268b28232bede5f868087cc1
Audrius Siliunas <audrius.siliunas@tietocamp.eu>
2014-08-18 15:57:14 +0300

    modal window style merge fix

--- it2014-ui/src/main/java/com/tieto/it2014/ui/friend/FriendItemPanel.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/friend/FriendItemPanel.java
@@ -1,24 +1,22 @@
 package com.tieto.it2014.ui.friend;
 
-import com.tieto.it2014.domain.user.command.DeleteFriendCommand;
 import com.tieto.it2014.domain.user.entity.User;
 import com.tieto.it2014.ui.HomePage;
-import com.tieto.it2014.ui.session.UserSession;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class FriendItemPanel extends Panel {
 
     private static final long serialVersionUID = 1L;
 
     private final User friend;
-
-    @SpringBean
-    private DeleteFriendCommand deleteFriendCommand;
+    private ModalWindow modalWindow;
 
     public FriendItemPanel(String id, User friend) {
         super(id);
@@ -37,19 +35,32 @@ public class FriendItemPanel extends Panel {
         l.add(new Label("friendLabel", friend.username));
         l.add(initDeleteButton("friendDelete"));
 
+        modalWindow = new ModalWindow("confirmModalWindow");
+
+        modalWindow.showUnloadConfirmation(false);
+        modalWindow.setResizable(true);
+        modalWindow.setAutoSize(false);
+
+        modalWindow.setWidthUnit("px");
+        modalWindow.setHeightUnit("px");
+        modalWindow.setInitialHeight(200);
+        modalWindow.setInitialWidth(500);
+        modalWindow.setMinimalHeight(200);
+        modalWindow.setMinimalWidth(500);
+        modalWindow.setContent(new ConfirmDeleteFriendPanel(modalWindow.getContentId(),
+                friend, modalWindow));
+
+        add(modalWindow);
         add(l);
     }
 
     private Link initDeleteButton(String wicketId) {
-        return new Link(wicketId) {
+        return new AjaxFallbackLink(wicketId) {
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void onClick() {
-                deleteFriendCommand.execute(UserSession.get().getUser().imei,
-                        friend.imei);
-
-                setResponsePage(HomePage.class);
+            public void onClick(AjaxRequestTarget target) {
+                modalWindow.show(target);
             }
 
         };

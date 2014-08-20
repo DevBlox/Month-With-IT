0663ed9dc3f18b0e965a4c09fc4d7804780fc86c
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-20 14:05:31 +0300

    centai

--- it2014-dao/src/main/java/com/tieto/it2014/dao/weight/query/AllWeightQueryDaoJpa.java
+++ it2014-dao/src/main/java/com/tieto/it2014/dao/weight/query/AllWeightQueryDaoJpa.java
@@ -2,15 +2,27 @@ package com.tieto.it2014.dao.weight.query;
 
 import com.tieto.it2014.domain.weight.entity.Weight;
 import com.tieto.it2014.domain.weight.query.WeightQuery;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by mantas on 20/08/14.
  */
+@Component
 public class AllWeightQueryDaoJpa implements WeightQuery.Dao {
+
+    private static final long serialVersionUID = 1L;
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional(readOnly = true)
     public List<Weight> result(String imei) {
         List<Weight> lw = new ArrayList<>();
         lw.add(new Weight((float)50.5, "355866055632819", 1, 1408531889L));

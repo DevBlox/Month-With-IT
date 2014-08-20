3ec9d5304c61ccf69e72d61dd41f9529955157a8
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-20 09:03:59 +0300

    Empty weights page added.

---
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/user/WeightPage.java
@@ -0,0 +1,10 @@
+package com.tieto.it2014.ui.user;
+
+import com.tieto.it2014.ui.BasePage;
+
+/**
+ * Created by mantas on 20/08/14.
+ */
+public class WeightPage extends BasePage {
+    private static final long serialVersionUID = 1L;
+}

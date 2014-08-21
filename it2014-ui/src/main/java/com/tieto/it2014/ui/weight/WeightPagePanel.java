582d82bafe74b512f3fd46dee47f960cc0e19066
Mantas Jonytis <mantas.jonytis@tietocamp.eu>
2014-08-21 13:48:36 +0300

    centai.

--- it2014-ui/src/main/java/com/tieto/it2014/ui/weight/WeightPagePanel.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/weight/WeightPagePanel.java
@@ -8,6 +8,7 @@ import com.tieto.it2014.ui.weight.detail.ChartPanel;
 import com.tieto.it2014.ui.weight.detail.RandomQuote;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 public class WeightPagePanel extends Panel {
 
     private static final long serialVersionUID = 1L;
-    List<Weight> data;
+    private List<Weight> data;
 
     public WeightPagePanel(String id) {
         super(id);
@@ -27,11 +28,13 @@ public class WeightPagePanel extends Panel {
     @Override
     protected void onInitialize() {
         super.onInitialize();
+        add(new Label("quote", ""));
+        add(new Label("cite", ""));
         try {
-        String imei = UserSession.get().getUser().imei;
-        data = weightQuery.result(imei);
-        add(new WeightInputPanel("weightInput", data));
-        add(new ChartPanel("weightChart", data));
+            String imei = UserSession.get().getUser().imei;
+            data = weightQuery.result(imei);
+            add(new WeightInputPanel("weightInput", data));
+            add(new ChartPanel("weightChart", data));
         } catch (NullPointerException e) {
             setResponsePage(ErrorPage404.class);
         }
@@ -44,10 +47,12 @@ public class WeightPagePanel extends Panel {
     }
 
     public void addQuote() {
+        this.remove("quote");
+        this.remove("cite");
         String[] pos = RandomQuote.getPositive();
         String[] neg = RandomQuote.getNegative();
         if (!data.isEmpty()) {
-            if ((double) (Math.round(data.get(data.size() - 1).weight * 10)) / 10 >= 0) {
+            if (data.get(data.size()-1).weight-data.get(0).weight > 0) {
                 add(new Label("quote", neg[0]));
                 add(new Label("cite", neg[1]));
             } else {

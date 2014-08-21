d086c75f16fa0929dfb163a93ffd039bf12c9d44
Lukas Praninskas <lukas.praninskas@tietocamp.eu>
2014-08-21 18:36:42 +0300

    weight by period query and dao

---
+++ it2014-dao/src/test/java/weight/query/UserWeightOverPeriodQueryTest.java
@@ -0,0 +1,21 @@
+//package weight.query;
+//
+//import com.tieto.it2014.dao.BaseDaoTest;
+//import com.tieto.it2014.domain.user.entity.Workout;
+//import com.tieto.it2014.domain.weight.query.UserWeightOverPeriod;
+//import java.util.List;
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class UserWeightOverPeriodQueryTest extends BaseDaoTest {
+//
+//    @Autowired
+//    private UserWeightOverPeriod weightOverPeriodQuery;
+//
+//    @Test
+//    public void retrieves_things() {
+//        List<Workout> testList;
+//        testList = weightOverPeriodQuery.result(start, finish, , type)
+//    }
+//
+//}

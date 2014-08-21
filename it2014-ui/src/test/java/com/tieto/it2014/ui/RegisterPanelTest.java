6634b278fab46bd5e0dcdfc585109b7be57330a7
Audrius Siliunas <audrius.siliunas@tietocamp.eu>
2014-08-21 09:08:16 +0300

    centai

--- it2014-ui/src/test/java/com/tieto/it2014/ui/RegisterPanelTest.java
+++ it2014-ui/src/test/java/com/tieto/it2014/ui/RegisterPanelTest.java
@@ -3,24 +3,13 @@ package com.tieto.it2014.ui;
 import com.tieto.it2014.domain.user.query.GetUserByIdQuery;
 import com.tieto.it2014.ui.validation.ExistingImeiValidator;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.util.tester.WicketTester;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidationError;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.test.context.web.ServletTestExecutionListener;
 
 
 

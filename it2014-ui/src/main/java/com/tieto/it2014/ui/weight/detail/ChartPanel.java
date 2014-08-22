0de79485ae11965b2ec26dafa881bf43b9004e55
Audrius Siliunas <audrius.siliunas@tietocamp.eu>
2014-08-22 10:14:07 +0300

    weight chart input form long

--- it2014-ui/src/main/java/com/tieto/it2014/ui/weight/detail/ChartPanel.java
+++ it2014-ui/src/main/java/com/tieto/it2014/ui/weight/detail/ChartPanel.java
@@ -1,15 +1,21 @@
 package com.tieto.it2014.ui.weight.detail;
 
+import static com.google.common.collect.ComparisonChain.start;
 import com.googlecode.wickedcharts.highcharts.options.Options;
 import com.googlecode.wickedcharts.wicket6.highcharts.Chart;
 import com.tieto.it2014.domain.weight.entity.Weight;
 import com.tieto.it2014.ui.user.WeightPage;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
@@ -41,6 +47,14 @@ public class ChartPanel extends Panel {
 //        "1", "2", "3"});
 //    private List<String> daysInMonth = getDaysInThisMonth();
     private String selected = "1";
+    private DropDownChoice<String> listDays;
+    private DropDownChoice<String> listMonths;
+    private DropDownChoice<String> listYears;
+    private DropDownChoice<String> listQuarters;
+    private Form<?> daysForm;
+    private Form<?> monthForm;
+    private Form<?> yearForm;
+    private Form<?> quarterForm;
 
     private Options options = new Options();
     private Chart chart;
@@ -118,14 +132,52 @@ public class ChartPanel extends Panel {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-
+        listDays = new DropDownChoice<String>(
+                "days", new PropertyModel<String>(this, "selected"), getDaysInThisMonth());
+        listMonths = new DropDownChoice<String>(
+                "days", new PropertyModel<String>(this, "selected"), getMonthsInThisYear());
+//        listYears = new DropDownChoice<String>(
+//                "days", new PropertyModel<String>(this, "selected"), getYe());
+        listQuarters = new DropDownChoice<String>(
+                "days", new PropertyModel<String>(this, "selected"), getQuartersInThisYear());
         options = ChartPanelOptionsProvider.getInstance().getOptions();
         chart = new Chart("chart", options);
-        
-        DropDownChoice<String> listDays = new DropDownChoice<String>(
-                "days", new PropertyModel<String>(this, "selected"), getDaysInThisMonth());
 
-        Form<?> daysForm = new Form<Void>("dropDownForm") {
+//        DropDownChoice<String> listDays = new DropDownChoice<String>(
+//                "days", new PropertyModel<String>(this, "selected"), getDaysInThisMonth());
+        daysForm = new Form<Void>("dropDownForm") {
+            @Override
+            protected void onSubmit() {
+                Long start = 0L;
+                try {
+                    start = createTimeStamp(getYearString(), getMonthString(), selected);
+                } catch (ParseException ex) {
+                    Logger.getLogger(ChartPanel.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                int endInt = Integer.parseInt(selected) + 1;
+                String endString = Integer.toString(endInt);
+                Long end = 0L;
+                try {
+                    end = createTimeStamp(getYearString(), getMonthString(), endString);
+                } catch (ParseException ex) {
+                    Logger.getLogger(ChartPanel.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                ChartPanelOptionsProvider.getInstance().getGivenTimeOptions(start, end);
+                info("Jūs pasirinkote : " + selected);
+
+            }
+        };
+
+        monthForm = new Form<Void>("dropDownFormMonth") {
+            @Override
+            protected void onSubmit() {
+
+                info("Jūs pasirinkote : " + selected);
+
+            }
+        };
+
+        quarterForm = new Form<Void>("dropDownFormQuarter") {
             @Override
             protected void onSubmit() {
 
@@ -152,6 +204,10 @@ public class ChartPanel extends Panel {
         // int kokia forma vaizduoti
         ChartPanelOptionsProvider.getInstance().getOptionsType();
         add(new FeedbackPanel("feedback"));
+        add(quarterForm);
+        quarterForm.add(listQuarters);
+        add(monthForm);
+        monthForm.add(listMonths);
         add(daysForm);
         daysForm.add(listDays);
         add(chart);
@@ -164,12 +220,26 @@ public class ChartPanel extends Panel {
         super.onConfigure();
         options = ChartPanelOptionsProvider.getInstance().getOptions();
         chart.setOptions(options);
+        hideForms(ChartPanelOptionsProvider.getInstance().getOptionsType());
     }
 
     private void hideForms(int type) {
         switch (type) {
             case BUTTON_TYPE_DAY:
-
+                daysForm.setVisible(true);
+                monthForm.setVisible(false);
+                quarterForm.setVisible(false);
+                break;
+            case BUTTON_TYPE_MONTH:
+                monthForm.setVisible(true);
+                daysForm.setVisible(false);
+                quarterForm.setVisible(false);
+                break;
+            case BUTTON_TYPE_QUARTER:
+                monthForm.setVisible(false);
+                daysForm.setVisible(false);
+                quarterForm.setVisible(true);
+                break;
         }
     }
 
@@ -187,11 +257,22 @@ public class ChartPanel extends Panel {
         return createdListOfdays;
     }
 
+    private String getMonthString() {
+         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+         return Integer.toString(calendar.get(Calendar.MONTH) + 1);
+    }
+    
+    private String getYearString() {
+         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+         return Integer.toString(calendar.get(Calendar.YEAR));
+    }
+    
+
     private List<String> getMonthsInThisYear() {
 
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         // Date date = calendar.getTime();
-        int month = calendar.get(Calendar.MONTH)+1;
+        int month = calendar.get(Calendar.MONTH) + 1;
         String[] stringArrayOfMonths = new String[month];
         for (int i = 1; i <= month; i++) {
             stringArrayOfMonths[i - 1] = Integer.toString(i);
@@ -199,14 +280,14 @@ public class ChartPanel extends Panel {
         ArrayList<String> createdListOfMonths = new ArrayList<String>(Arrays.asList(stringArrayOfMonths));
         return createdListOfMonths;
     }
-   
+
     private List<String> getQuartersInThisYear() {
 
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         // Date date = calendar.getTime();
-        int month = calendar.get(Calendar.MONTH)+1;
+        int month = calendar.get(Calendar.MONTH) + 1;
         int quarters = month / 3;
-        if ((month % 3) !=0) {
+        if ((month % 3) != 0) {
             quarters = quarters + 1;
         }
         String[] stringArrayOfQuarters = new String[quarters];
@@ -215,6 +296,14 @@ public class ChartPanel extends Panel {
         };
         ArrayList<String> createdListOfQuarters = new ArrayList<String>(Arrays.asList(stringArrayOfQuarters));
         return createdListOfQuarters;
+    }
+
+    private Long createTimeStamp(String year, String month, String day) throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = day + "/" + month + "/" + year;
+        Date date = dateFormat.parse(dateString);
+        long timestamp = date.getTime();
+        return timestamp;
     }
 
 }

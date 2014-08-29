bcad12a32364079d68a0c9e3d531167d71e12aba
pc1 <pc1@Demo10-Optiplex755.(none)>
2014-08-29 11:28:43 +0300

    setters getters

--- it2014-domain/src/main/java/com/tieto/it2014/domain/user/entity/UserStats.java
+++ it2014-domain/src/main/java/com/tieto/it2014/domain/user/entity/UserStats.java
@@ -10,11 +10,11 @@ public class UserStats extends Entity {
 
     private static final long serialVersionUID = 1L;
 
-    public Integer topId;
-    public String userName;
-    public Double distance;
-    public String time;
-    public Double avgSpeed;
+    private Integer topId;
+    private String userName;
+    private Double distance;
+    private String time;
+    private Double avgSpeed;
 
     public UserStats(String id, String userName, Double distance, Integer time, Double avgSpeed) {
         super(id);
@@ -24,6 +24,72 @@ public class UserStats extends Entity {
         this.avgSpeed = avgSpeed;
     }
 
+    /**
+     * @return the topId
+     */
+    public Integer getTopId() {
+        return topId;
+    }
+
+    /**
+     * @param topId the topId to set
+     */
+    public void setTopId(Integer topId) {
+        this.topId = topId;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @param distance the distance to set
+     */
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    /**
+     * @return the time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the avgSpeed
+     */
+    public Double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    /**
+     * @param avgSpeed the avgSpeed to set
+     */
+    public void setAvgSpeed(Double avgSpeed) {
+        this.avgSpeed = avgSpeed;
+    }
+
+    /**
+     * @return the distance
+     */
     public Double getDistance() {
         return distance;
     }
